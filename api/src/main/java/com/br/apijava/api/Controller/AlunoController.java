4a8556e9ef025728d1467bd3b0675c143f274d47
Bruno Wagler Cruz Padilha Ferreira <reidx0@Outlook.com>
2025-06-25 20:13:25 -0300

    metodo delete

--- api/src/main/java/com/br/apijava/api/Controller/AlunoController.java
+++ api/src/main/java/com/br/apijava/api/Controller/AlunoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +50,17 @@ public class AlunoController
     {
         return alunoRepository.findAll();
     }
+
+    
+    @DeleteMapping("/deletar/{id}")
+    public String deletar(@PathVariable Long id) 
+    {
+        alunoRepository.deleteById(id);
+        return "Aluno deletado com sucesso";
+    }
+
+    
+
 
     
 }
--- api/src/main/java/com/br/apijava/api/Controller/PessoaController.java
+++ api/src/main/java/com/br/apijava/api/Controller/PessoaController.java
@@ -2,7 +2,9 @@ package com.br.apijava.api.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,4 +37,15 @@ public class PessoaController {
     {
         return repository.save(novaPessoa);
     }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletar(@PathVariable Long id) 
+    {
+        repository.deleteById(id);
+        return "deletado com sucesso";
+    }
+
+    
+
+
 }
