62024733e4b88c008020acb19b1be0818df01ac4
Bruno Wagler Cruz Padilha Ferreira <reidx0@Outlook.com>
2025-07-01 15:48:02 -0300

    As funções únicas de busca pelo banco

--- api/src/main/java/com/br/apijava/api/Repository/AlunoRepository.java
+++ api/src/main/java/com/br/apijava/api/Repository/AlunoRepository.java
@@ -1,10 +1,23 @@
 package com.br.apijava.api.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.br.apijava.api.Model.AlunoModel;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
+public interface AlunoRepository extends JpaRepository<AlunoModel, Long> 
+{
+    @Query("SELECT a FROM AlunoModel a WHERE a.pessoa.nomeEstudante like %:nome%")
+    List<AlunoModel> findByNomeEstudante(String nome);
+
+    @Query("SELECT a FROM AlunoModel a WHERE a.campus like %:campus%")
+    List<AlunoModel> findByCampus(String campus);
+
+    @Query("SELECT a FROM AlunoModel a WHERE a.curso like %:curso%")
+    List<AlunoModel> findByCurso(String curso);
+
 }
