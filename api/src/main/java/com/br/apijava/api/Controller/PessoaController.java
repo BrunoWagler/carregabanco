fd3bc1a56cfca679a30ba612397008af9ae5187d
Bruno Wagler Cruz Padilha Ferreira <reidx0@Outlook.com>
2025-07-01 15:46:58 -0300

    Metodo atualizar inserido

--- api/src/main/java/com/br/apijava/api/Controller/PessoaController.java
+++ api/src/main/java/com/br/apijava/api/Controller/PessoaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,31 +22,36 @@ public class PessoaController {
 
     private final PessoaRepository repository;
 
-    public PessoaController(PessoaRepository repository) 
-    {
+    public PessoaController(PessoaRepository repository) {
         this.repository = repository;
     }
 
     @GetMapping("/todos")
-    public List<PessoaModel> getAll() 
-    {
+    public List<PessoaModel> getAll() {
         return repository.findAll();
     }
 
     @PostMapping("/inserir")
-    public PessoaModel inserir(@RequestBody @Valid PessoaModel novaPessoa) 
-    {
+    public PessoaModel inserir(@RequestBody @Valid PessoaModel novaPessoa) {
         return repository.save(novaPessoa);
     }
 
     @DeleteMapping("/deletar/{id}")
-    public String deletar(@PathVariable Long id) 
-    {
+    public String deletar(@PathVariable Long id) {
         repository.deleteById(id);
         return "deletado com sucesso";
     }
 
-    
+    @PutMapping("/atualizar/{id}")
+    public PessoaModel atualizar(@PathVariable Long id, @RequestBody @Valid PessoaModel pessoaAtualizada) {
+        PessoaModel pessoaExistente = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Pessoa não encontrada com o id: " + id));
 
+        pessoaExistente.setNomeEstudante(pessoaAtualizada.getNomeEstudante());
+        pessoaExistente.setIdade(pessoaAtualizada.getIdade());
+        pessoaExistente.setSexo(pessoaAtualizada.getSexo());
+
+        return repository.save(pessoaExistente);
+    }
 
 }
